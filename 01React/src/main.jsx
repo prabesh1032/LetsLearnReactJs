8200314a420c130bbbf537d905cfd9ac475db49a
Prabesh Acharya <praveshach1032@gmail.com>
2026-02-02 17:02:25 +0545

    understanding react hook ,use state

--- 01React/src/main.jsx
+++ 01React/src/main.jsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { App2, App3 } from './App.jsx'
+import CounDownApp from './CounDownApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <App2 />
-    <App3 />
+   <CounDownApp />
   </StrictMode>,
 )
